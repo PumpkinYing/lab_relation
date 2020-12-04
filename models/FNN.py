b14d79f95e5e73f27f134e6fec4a8b40b6c56bc0
ying971101 <137339291@qq.com>
2020-12-04 16:21:35 +0800

    update

---
+++ models/FNN.py
@@ -0,0 +1,16 @@
+import torch 
+import torch.nn.functional as F 
+
+class FNN(torch.nn.Module) :
+    def __init__(self, in_features, out_features, layer_num, node_num):
+        super(FNN, self).__init__()
+        self.layers = []
+        self.layers.append(nn.Linear(in_features, node_num))
+        for num in range(layer_num-1):
+            self.layers.append(nn.Linear(node_num, node_num))
+        self.layers.append(nn.Linear(node_num, out_features))
+    
+    def forward(self, x):
+        for layer in self.layers :
+            x = layer(x)
+        return F.relu(x)
